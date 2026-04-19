f67ac41a086b344658ab2264dd32fe2f71a26c8c
yakujaae-afk <yakujaae@gmail.com>
2026-04-18 18:49:35 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
-# Don't Remove Credit Tg - @newstudent1885
-# Ask Doubt on telegram @newstudent1885
+import os
 
-from os import environ
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-API_ID = int(environ.get("API_ID", "30296254")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "8695529823:AAGjnMQdp8x162jrpOts0wqWYol_ujmpgBQ") #Replace with your bot token
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8870))  # Default to 8000 if not set
