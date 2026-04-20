9d6fa1eb526a418a4ea42488daabfefb641f0582
yakujaae-afk <yakujaae@gmail.com>
2026-04-19 22:01:52 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID    = os.environ.get("30296254", "")
-API_HASH  = os.environ.get("c2b5306f4ccd2d795405a026c10b4c62", "")
-BOT_TOKEN = os.environ.get("8747255628:AAEqW2TJdoerOgpf2eb_Wqh49zPhCjs4O44", "") 
+API_ID    = os.environ.get("", "")
+API_HASH  = os.environ.get("", "")
+BOT_TOKEN = os.environ.get("", "") 
 
