01a9cd1527956caf9d285ccdadeca93a051928d2
yakujaae-afk <yakujaae@gmail.com>
2026-04-27 23:21:55 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,5 @@
-# Don't Remove Credit Tg - @newstudent1885
-# Ask Doubt on telegram @newstudent1885
+import os
 
-from os import environ
-
-API_ID = int(environ.get("API_ID", "30296254"))
-API_HASH = environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62") 
-BOT_TOKEN = environ.get("BOT_TOKEN", "8721294227:AAHhVYXkxVML6zuy70fKJe1Bljcut3-Yt4A") 
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
